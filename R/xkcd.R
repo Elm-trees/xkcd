b67719cd8118e853406bafde516d0eea12024a8d
Elm-trees <emarkert@smith.edu>
2024-04-05 09:07:27 -0400

    Added xkcd() function to pull json information as well as documentation and dependencies.

---
+++ R/xkcd.R
@@ -0,0 +1,13 @@
+#' @title Retrieve metadata about an XKCD comic
+#'
+#' @description
+#' Given an xkcd comic number, this function retrieves a JSON object describing that comic from the official xkcd API.
+#'
+#' @importFrom jsonlite read_json
+#'
+#' @export
+
+xkcd <- function(number) {
+  url <- file.path("https://xkcd.com", number, "info.0.json")
+  results <- jsonlite::read_json(url)
+}
